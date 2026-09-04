adaf74a786ce3c670c1240a01632b18820c87f87
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-09-04 16:37:33 +0300

    created entities

---
+++ CarBook/Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,23 @@
+﻿using CarBook.Domain.Enums;
+
+namespace CarBook.Domain.Entities
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int Km { get; set; }
+        public TransmissionType Transmission { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public FuelType Fuel { get; set; }
+        public string BigImageUrl { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+        public int CarDescriptionId { get; set; }
+        public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
+    }
+}
---
+++ CarBook/Core/CarBook.Domain/Enums/FuelType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Enums
+{
+    public enum FuelType
+    {
+        Gasoline = 1,
+        Diesel = 2,
+        Electric = 3,
+        Hybrid = 4,
+        Lpg = 5
+    }
+}
