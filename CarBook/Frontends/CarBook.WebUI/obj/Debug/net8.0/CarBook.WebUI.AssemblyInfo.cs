4fbbbcd4e977f6b9b86b276f58a2cfa239a2826e
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-09-14 23:42:09 +0300

    updated: sidebar link for statistics

--- CarBook/Frontends/CarBook.WebUI/obj/Debug/net8.0/CarBook.WebUI.AssemblyInfo.cs
+++ CarBook/Frontends/CarBook.WebUI/obj/Debug/net8.0/CarBook.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab8828ad16ada071888b189a4385ca6f3637af1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6592ecdfd58e7dbf4c50c053ac02a22200605334")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
